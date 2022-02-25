d1bf491d5b25f6c94354c775c52f076b6ecdfaf4
Dan Schultz <slifty@gmail.com>
2022-02-25 15:26:37 -0500

    Add enum typeguard generation utility
    Enums are special in TypeScript in that they are converted to actual
    usable runtime objects.
    
    It will be a common use case in this project to need to take a string
    (e.g. an API return value) and type it to one of our many enum values.
    This utility function will prevent us from having to repeat the same
    "keyof" / object meta logic for every single one of our typeguards.

---
+++ src/utils/generateEnumTypeguard.ts
@@ -0,0 +1,8 @@
+// This is a modified version of code written by StackOverflow user @jcalz
+// https://stackoverflow.com/a/58278753/159522
+export const generateEnumTypeguard = <GenericEnum>(genericEnum: GenericEnum) =>
+  (value: unknown): value is GenericEnum[keyof GenericEnum] => {
+    const token = value as GenericEnum[keyof GenericEnum];
+    const validTokens = Object.values(genericEnum);
+    return validTokens.includes(token);
+  };
