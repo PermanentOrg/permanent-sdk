6bab5257e437f5c0e39b26d053319c2030066a86
fenn-cs <fenn25.fn@gmail.com>
2022-04-16 00:07:03 +0100

    Add FolderVo type definitions
    In FolderVo.ts only four important properties are defined which are
    folderId, displayName & archiveId which are both marked as required
    in the backend and displayDT required for sorting in the SDK.
    
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- src/types/api/FolderVo.ts
+++ src/types/api/FolderVo.ts
@@ -1,3 +1,17 @@
-// This is a placeholder interface which will ultimately be populated
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface FolderVo {}
+export interface FolderVo {
+  // These fields map to those defined in the base library FolderVO
+  // https://github.com/PermanentOrg/back-end/blob/main/api/core/folder/vo/folder.vo.php
+  folderId: number;
+  displayName: string;
+  displayDT: string;
+  createdDT: string;
+  updatedDT: string;
+}
+
+export const defaultFolderVo: FolderVo = {
+  folderId: 0,
+  displayName: '',
+  displayDT: '',
+  createdDT: '',
+  updatedDT: '',
+};
