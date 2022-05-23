faa11cf58af5ee67b94c78d004df0908c08bddbc
fenn-cs <fenn25.fn@gmail.com>
2022-05-24 00:51:13 +0100

    Add base properties for sdk Folder.ts type
    Signed-off-by: fenn-cs <fenn25.fn@gmail.com>

--- src/types/sdk/Folder.ts
+++ src/types/sdk/Folder.ts
@@ -1,3 +1,12 @@
+import type { Item } from './Item';
+
 export interface Folder {
+  id: number;
+  name: string;
   size: number;
+  displayDate: Date;
+  readonly folders: Folder[];
+  readonly items: Item[];
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
 }
