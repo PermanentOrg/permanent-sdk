9fdfa03ccf41cd422ec4a90d7ccb06ddcf1e72e7
Fenn-CS <fenn25.fn@gmail.com>
2022-07-04 23:28:10 +0100

    Add FolderVo to Folder conversion utility
    Signed-off-by: Fenn-CS <fenn25.fn@gmail.com>

---
+++ src/utils/folderVoToFolder.ts
@@ -0,0 +1,20 @@
+import type {
+  FolderVo,
+  Folder,
+} from '../types';
+
+export const folderVoToFolder = (folderVo: FolderVo): Folder => {
+  const createdAt = new Date(folderVo.createdDT); 
+  const updatedAt = new Date(folderVo.updatedDT);
+  const displayDate = new Date(folderVo.displayDT);
+  return {
+    id: folderVo.folderId,
+    name: folderVo.displayName,
+    size: 0,
+    createdAt,
+    updatedAt,
+    displayDate,
+    folders: [],
+    items: [],
+  };
+};
