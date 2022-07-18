1e9a8eca7c6679a37968b94f2a344b5a4355b338
Dan Schultz <slifty@gmail.com>
2022-07-18 13:05:10 -0400

    Add getArchiveFolders
    This adds a `getArchiveFolders` sdk function which will return the first
    level of folders contained in an archive.  This implementation hides the
    concept of a "root" folder from SDK user; the root folder exists as the
    folder representation of an archive in the back end.
    
    Issue #43

--- src/utils/folderVoToFolder.ts
+++ src/utils/folderVoToFolder.ts
@@ -1,20 +1,16 @@
+import { formatTimestampAsUtc } from './formatTimestampAsUtc';
 import type {
   FolderVo,
   Folder,
 } from '../types';
 
-export const folderVoToFolder = (folderVo: FolderVo): Folder => {
-  const createdAt = new Date(folderVo.createdDT);
-  const updatedAt = new Date(folderVo.updatedDT);
-  const displayDate = new Date(folderVo.displayDT);
-  return {
-    id: folderVo.folderId,
-    name: folderVo.displayName,
-    size: 0,
-    createdAt,
-    updatedAt,
-    displayDate,
-    folders: [],
-    records: [],
-  };
-};
+export const folderVoToFolder = (folderVo: FolderVo): Folder => ({
+  id: folderVo.folderId,
+  name: folderVo.displayName,
+  size: 0,
+  createdAt: new Date(formatTimestampAsUtc(folderVo.createdDT)),
+  updatedAt: new Date(formatTimestampAsUtc(folderVo.updatedDT)),
+  displayDate: new Date(formatTimestampAsUtc(folderVo.displayDT)),
+  folders: [],
+  records: [],
+});
