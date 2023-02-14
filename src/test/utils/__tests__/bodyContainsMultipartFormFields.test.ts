872c4e3788730d9dd1e85960d8e6efec81e99492
Dan Schultz <slifty@gmail.com>
2023-02-14 13:17:44 -0500

    Add multipart form test utility
    Multipart forms are a huge pain to test against and for whatever reason
    it looks like nock doesn't support multipart body parsing out of the
    box.  This little utility method should, in theory, be able to compare
    a multipart form body with a list of key / value pairs and at least
    confirm that each pair exists in the body somewhere.
    
    This will only be used for unit tests.

---
+++ src/test/utils/__tests__/bodyContainsMultipartFormFields.test.ts
@@ -0,0 +1,66 @@
+import { bodyContainsMultipartFormFields } from '..';
+
+describe('bodyContainsMultipartFormFields', () => {
+  it('should match a valid body ', async () => {
+    const body = `----------------------------369405283061745602785268
+Content-Disposition: form-data; name="firstField"
+
+foo
+----------------------------369405283061745602785268
+Content-Disposition: form-data; name="secondField"
+
+bar
+----------------------------369405283061745602785268
+Content-Disposition: form-data; name="file"
+Content-Type: application/octet-stream
+
+hello`;
+    const expectedFields = {
+      firstField: 'foo',
+      secondField: 'bar',
+    };
+    expect(bodyContainsMultipartFormFields(
+      body,
+      expectedFields,
+    )).toBe(true);
+  });
+
+  it('should not match fields values with future field values', async () => {
+    const body = `----------------------------369405283061745602785268
+Content-Disposition: form-data; name="firstField"
+
+bar
+----------------------------369405283061745602785268
+Content-Disposition: form-data; name="secondField"
+
+foo
+----------------------------369405283061745602785268
+Content-Disposition: form-data; name="file"
+Content-Type: application/octet-stream
+
+hello`;
+    const expectedFields = {
+      firstField: 'foo',
+      secondField: 'foo',
+    };
+    expect(bodyContainsMultipartFormFields(
+      body,
+      expectedFields,
+    )).toBe(false);
+  });
+
+  it('should not match a body that is not a string', async () => {
+    const body = {
+      firstField: 'foo',
+      secondField: 'foo',
+    };
+    const expectedFields = {
+      firstField: 'foo',
+      secondField: 'foo',
+    };
+    expect(bodyContainsMultipartFormFields(
+      body,
+      expectedFields,
+    )).toBe(false);
+  });
+});
