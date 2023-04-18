cbcb0a12fae6e4c9db8c399369eac291b6a6cca8
Dan Schultz <slifty@gmail.com>
2023-04-18 12:01:28 -0400

    Populate `isSftpDeletionEnabled` property
    The Permanent API has been modified to provide a user setting that
    ultimately maps to our `isSftpDeletionEnabled` property.  We had
    pre-emptively included the property, knowing this development was
    incoming.
    
    Issue #96 Read sftp deletion setting

--- src/utils/__tests__/accountVoToAccount.test.ts
+++ src/utils/__tests__/accountVoToAccount.test.ts
@@ -4,13 +4,14 @@ describe('accountVoToAccount', () => {
   it('should properly populate an Account based on AccountVo data', () => {
     const accountVo = {
       accountId: 0,
+      allowSftpDeletion: true,
       createdDT: '2022-07-10T14:59:03.853Z',
       updatedDT: '2022-08-10T14:59:03.853Z',
     };
     const account = accountVoToAccount(accountVo);
     expect(account).toEqual({
       id: 0,
-      isSftpDeletionEnabled: false,
+      isSftpDeletionEnabled: true,
       createdAt: new Date('2022-07-10T14:59:03.853Z'),
       updatedAt: new Date('2022-08-10T14:59:03.853Z'),
     });
