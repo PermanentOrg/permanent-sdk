5b7071481f2b4a412155361f20cbe2bb7e268456
Dan Schultz <slifty@gmail.com>
2023-08-09 11:50:30 -0400

    Fix enum typeguard generator
    GenericEnum wasn't necessarily an object (though that is the intent)
    and the latest typescript correctly flagged the potential mistake from
    not specifying that expectation.

--- src/utils/generateEnumTypeguard.ts
+++ src/utils/generateEnumTypeguard.ts
@@ -1,6 +1,6 @@
 // This is a modified version of code written by StackOverflow user @jcalz
 // https://stackoverflow.com/a/58278753/159522
-export const generateEnumTypeguard = <GenericEnum>(genericEnum: GenericEnum) => (
+export const generateEnumTypeguard = <GenericEnum extends object>(genericEnum: GenericEnum) => (
   (value: unknown): value is GenericEnum[keyof GenericEnum] => {
     const token = value as GenericEnum[keyof GenericEnum];
     const validTokens = Object.values(genericEnum);
