1fb23e386a699a8e26a9636297b103ca0307db98
Dan Schultz <slifty@gmail.com>
2025-05-29 11:11:23 -0400

    Support additional retry configuration
    The retry library we are using allows configuration of number of retry
    attempts in addition to a custom delay function.  Exposing these
    configurable values gives SDK users more control according to their use
    cases.
    
    Issue #522 Expose more configuration settings for retry logic

--- src/utils/__tests__/makePermanentApiCall.test.ts
+++ src/utils/__tests__/makePermanentApiCall.test.ts
@@ -82,4 +82,39 @@ describe('makePermanentApiCall', () => {
     expect(responseBody).toEqual('it worked');
     expect(mock.isDone()).toBe(true); // All mocked responses were invoked
   });
+
+  it('should retry additional times than the default if configured to do so', async () => {
+    const mock = nock('https://permanent.local', {
+      reqheaders: {
+        Authorization: 'Bearer 12345',
+        'Request-Version': '2',
+        'Content-Type': 'application/json',
+      },
+    })
+      .get('/api/testing')
+      .reply(404, 'it did not work')
+      .get('/api/testing')
+      .reply(404, 'it did not work')
+      .get('/api/testing')
+      .reply(404, 'it did not work')
+      .get('/api/testing')
+      .reply(404, 'it did not work')
+      .get('/api/testing')
+      .reply(200, 'it worked');
+
+    const response = await makePermanentApiCall(
+      {
+        bearerToken: '12345',
+        baseUrl: 'https://permanent.local/api',
+        retryOn: [404],
+        retries: 4,
+        retryDelay: (attempt) => attempt * 0.01,
+      },
+      '/testing',
+    );
+    const responseBody = await response.text();
+
+    expect(responseBody).toEqual('it worked');
+    expect(mock.isDone()).toBe(true); // All mocked responses were invoked
+  });
 });
