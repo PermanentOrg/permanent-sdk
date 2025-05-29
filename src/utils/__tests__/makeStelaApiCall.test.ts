b0d6ab4bf818f938cda7aebc0e3869f6aef27cae
Dan Schultz <slifty@gmail.com>
2025-05-29 11:11:23 -0400

    Support additional retry configuration
    The retry library we are using allows configuration of number of retry
    attempts in addition to a custom delay function.  Exposing these
    configurable values gives SDK users more control according to their use
    cases.
    
    Issue #522 Expose more configuration settings for retry logic

--- src/utils/__tests__/makeStelaApiCall.test.ts
+++ src/utils/__tests__/makeStelaApiCall.test.ts
@@ -92,4 +92,33 @@ describe('makeStelaApiCall', () => {
     expect(responseBody).toEqual('it worked');
     expect(mock.isDone()).toBe(true); // All mocked responses were invoked
   });
+
+  it('should retry additional times than the default if configured to do so', async () => {
+    const mock = nock('https://api.permanent.local')
+      .get('/api/v2/testing')
+      .reply(404, 'it did not work')
+      .get('/api/v2/testing')
+      .reply(404, 'it did not work')
+      .get('/api/v2/testing')
+      .reply(404, 'it did not work')
+      .get('/api/v2/testing')
+      .reply(404, 'it did not work')
+      .get('/api/v2/testing')
+      .reply(200, 'it worked');
+
+    const response = await makeStelaApiCall(
+      {
+        bearerToken: '12345',
+        stelaBaseUrl: 'https://api.permanent.local/api/v2',
+        retryOn: [404],
+        retries: 4,
+        retryDelay: (attempt) => attempt * 0.01,
+      },
+      '/testing',
+    );
+    const responseBody = await response.text();
+
+    expect(responseBody).toEqual('it worked');
+    expect(mock.isDone()).toBe(true); // All mocked responses were invoked
+  });
 });
--- src/utils/getFetch.ts
+++ src/utils/getFetch.ts
@@ -3,8 +3,8 @@ import fetchRetry from 'fetch-retry';
 import type { ClientConfiguration } from '../types/sdk/ClientConfiguration';
 
 const getFetch = (clientConfiguration: ClientConfiguration) => fetchRetry(originalFetch, {
-  retries: 3,
-  retryDelay: (attempt) => (2 ** attempt) * 1000,
+  retries: clientConfiguration.retries ?? 3,
+  retryDelay: clientConfiguration.retryDelay ?? ((attempt) => (2 ** attempt) * 1000),
   retryOn: clientConfiguration.retryOn ?? [],
 });
 
